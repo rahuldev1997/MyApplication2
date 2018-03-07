150170043d9a3c39bab1dbf942ee405f283a8292
parasthekoder <khandelwalparas8@gmail.com>
2018-03-07 16:12:34 +0530

    new patch

--- app/src/main/java/com/example/rahuldev/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rahuldev/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.rahuldev.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn = new Button(this);
-
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "created a patch", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
